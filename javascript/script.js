38bb7199cd93483c08dc549cd3fee61133be7fa7
JoaoBijos <joaopedrobijos96@gmail.com>
2020-11-26 11:27:17 -0300

    Update on the dice button

--- javascript/script.js
+++ javascript/script.js
@@ -7,13 +7,37 @@ var roundScore = 0;
 // variável para mostrar qual player é o da rodada
 var activePlayer = 0; // 0 = primeiro jogador, 1 = o segundo jogador 
 
-// criando o dado
-var dice = Math.floor(Math.random() * 6) + 1
+
 
 // pegando um elemento HTML e alterando o seu conteúdo para o conteúdo do dado
 // colocando o activePlayer, a ação será feita para quem estiver jogando
-document.querySelector('#current--' + activePlayer).textContent = dice; 
+// document.querySelector('#current--' + activePlayer).textContent = dice; 
 
 
 // pegamos a imagem do dado e fazemos com que ela não apareça quando o jogo for aberto
 document.querySelector('.dice').style.display = 'none';
+
+
+function btn(){
+    console.log('ksjbfkdjsa')
+    // 1 - precisamos de um número aleatório 
+    // criando o dado
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2 - mostrar o resultado
+    // recuperando o elemento HTML
+    var diceDOM =  document.querySelector('.dice');
+    // tornando ele visível
+    diceDOM.style.display = 'block';
+    // alternando as imagens do dado
+    diceDOM.src = './img/dice-' + dice + '.png';
+
+    // 3 - atualizar a pontuação da rodada se o número não for 1 
+}
+
+
+// fazendo a funcionalidade do botão roll dice
+// o addEventListener é o que vai dar as funcionalidades ao botão 
+// declaramos que o tipo do nosso evento será um 'click'
+// após isso declaramos qual funçào deverá ser realizada quando o evento acontecer
+document.querySelector('.btn--roll').addEventListener('click', btn)
